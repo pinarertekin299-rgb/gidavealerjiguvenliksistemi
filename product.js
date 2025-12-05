1cb26c045e0daad63b1169c8a5806f973ba77636
pinarertekin299-rgb <pinarertekin299@gmail.com>
2025-12-05 21:40:48 +0300

    product.js

--- product.js
+++ product.js
@@ -1,1 +1,137 @@
+/* -----------------------------------------------------
+   ÜRÜN VERİLERİ (10 ürün)
+----------------------------------------------------- */
+
+window.PRODUCTS = [
+  {
+    id: 1,
+    name: "Bisküvi X",
+    allergens: ["gluten", "milk"],
+    ingredients: "Buğday unu, şeker, süt tozu, kakao, yağ.",
+    nutrition: {
+      calories: 210,
+      sugar: "12g",
+      fat: "8g",
+      salt: "0.4g"
+    },
+    img: "img/1.jpg"
+  },
+  {
+    id: 2,
+    name: "Glütensiz Kraker",
+    allergens: [],
+    ingredients: "Pirinç unu, mısır nişastası, tuz.",
+    nutrition: {
+      calories: 140,
+      sugar: "2g",
+      fat: "3g",
+      salt: "0.2g"
+    },
+    img: "img/2.jpg"
+  },
+  {
+    id: 3,
+    name: "Fındıklı Çikolata",
+    allergens: ["nuts", "milk"],
+    ingredients: "Kakao, süt tozu, fındık, şeker.",
+    nutrition: {
+      calories: 230,
+      sugar: "20g",
+      fat: "14g",
+      salt: "0.1g"
+    },
+    img: "img/3.jpg"
+  },
+  {
+    id: 4,
+    name: "Sütlü Tatlı",
+    allergens: ["milk"],
+    ingredients: "Süt, şeker, kıvam arttırıcı.",
+    nutrition: {
+      calories: 180,
+      sugar: "16g",
+      fat: "5g",
+      salt: "0.2g"
+    },
+    img: "img/4.jpg"
+  },
+  {
+    id: 5,
+    name: "Yumurta Bazlı Kek",
+    allergens: ["egg", "gluten"],
+    ingredients: "Buğday unu, yumurta, şeker, kabartıcı.",
+    nutrition: {
+      calories: 260,
+      sugar: "22g",
+      fat: "10g",
+      salt: "0.3g"
+    },
+    img: "img/5.jpg"
+  },
+  {
+    id: 6,
+    name: "Soya Sosu",
+    allergens: ["soy"],
+    ingredients: "Soya fasulyesi, su, tuz.",
+    nutrition: {
+      calories: 50,
+      sugar: "3g",
+      fat: "0g",
+      salt: "3.0g"
+    },
+    img: "img/6.jpg"
+  },
+  {
+    id: 7,
+    name: "Pirinç Gofreti",
+    allergens: [],
+    ingredients: "Pirinç unu, tuz.",
+    nutrition: {
+      calories: 90,
+      sugar: "0g",
+      fat: "1g",
+      salt: "0.1g"
+    },
+    img: "img/7.jpg"
+  },
+  {
+    id: 8,
+    name: "Ekşi Maya Ekmeği",
+    allergens: ["gluten"],
+    ingredients: "Buğday unu, su, maya.",
+    nutrition: {
+      calories: 190,
+      sugar: "1g",
+      fat: "1g",
+      salt: "0.5g"
+    },
+    img: "img/8.jpg"
+  },
+  {
+    id: 9,
+    name: "Hazır Çorba",
+    allergens: ["gluten", "milk"],
+    ingredients: "Un, süt tozu, baharatlar, tuz.",
+    nutrition: {
+      calories: 110,
+      sugar: "4g",
+      fat: "2g",
+      salt: "1.2g"
+    },
+    img: "img/9.jpg"
+  },
+  {
+    id: 10,
+    name: "Kuruyemiş Karışımı",
+    allergens: ["nuts"],
+    ingredients: "Fındık, badem, kaju.",
+    nutrition: {
+      calories: 280,
+      sugar: "6g",
+      fat: "22g",
+      salt: "0.1g"
+    },
+    img: "img/10.jpg"
+  }
+];
 
